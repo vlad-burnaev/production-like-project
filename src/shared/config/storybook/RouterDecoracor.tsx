50413b75da591e9000bbb7e5fb56a7411b2268fc
Vladislav Burnaev <vlad.burn325@gmail.com>
2023-06-22 02:27:47 +0300

    loki setup

--- src/shared/config/storybook/RouterDecoracor.tsx
+++ src/shared/config/storybook/RouterDecoracor.tsx
@@ -1,9 +1,13 @@
 import { type StoryFn } from '@storybook/react'
 import { BrowserRouter } from "react-router-dom";
+import React, { Suspense } from "react";
+import { PageLoader } from "widgets/PageLoader";
 
 // eslint-disable-next-line react/display-name
 export const RouterDecoracor = () => (Story: StoryFn) => (
     <BrowserRouter>
-      <Story />
+      <Suspense fallback={<PageLoader />}>
+        <Story />
+      </Suspense>
     </BrowserRouter>
 )
